ac95fa4b51419868e84d598f621a4465b663000b
Natalia Ratnikova <natasha@mac-121252.dhcp.fnal.gov>
2020-02-26 18:41:42 -0600

    Added parsing functions from Adam Lyon's library.

--- fp2rm/conversion.py
+++ fp2rm/conversion.py
@@ -8,6 +8,7 @@ import sys
 import argparse
 import os.path
 import os
+import string
 
 class BadHTML(Exception):
     def __init__(self, value):
@@ -19,17 +20,20 @@ class WikiFromSoup:
     def __init__(self, soup, title, outfile):
         self.soup = clear_blank_lines(soup)
         self.title = title
-        self.textile = ''
+        self.textile = []
         self.outfile = outfile
+        self.isUlStack = []
+        self.liOffset = -2
     def addText(self, text):
         # encode as utf8 to avoid UnicodeEncodeError like this:
         # UnicodeEncodeError: 'ascii' codec can't encode character u'\xa0' in position 27: ordinal not in range(128) 
         res = text.encode('utf-8')
-        print "NR in addText adding:", res
-        self.outText.append(res)
+        print "NR in addText adding: \""+res+"\""
+        self.textile.append(res)
     def writeOutRedmine(self):
         ## Write this out 
-        open("%s" % self.outfile, 'w').write(self.textile+"\n")
+        text = string.join(self.textile, "")
+        open("%s" % self.outfile, 'w').write(text+"\n")
 
 ###  Functions to get HTML document contents:
 
@@ -70,6 +74,163 @@ def get_document_title (doc):
                     return s.string.strip()
             return None
 
+### Tag parsing functions adopted from Adam Lyon
+
+def parseTag(tagElement, o, offset):
+  tag = tagElement.name
+  print "NR in parseTag:", offset*"-" + tag
+
+  ## Handle the tag!
+  if tag == "a":
+    parseA(tagElement, o, offset)
+  elif tag in ["h1", "h2", "h3", "h4", "h5"]:
+    parseTextOut(tagElement, o, offset, "%s. " % tag, "\n\n")
+  elif tag in ["b", "strong"]:
+    o.addText('*%s*' % tagElement.get_text().strip())
+    #parseTextOut(tagElement, o, offset, "*", "*")
+  elif tag in ["code", "tt"]:
+    parseTextOut(tagElement, o, offset, " @", "@ ")
+  elif tag == "pre":
+    parseTextOut(tagElement, o, offset, "\n\n"+o.liOffset*" "+"<pre>\n", "\n"+" "*o.liOffset+"</pre>\n\n")
+  elif tag in ["p"]:
+    parseTextOut(tagElement, o, offset, "\n\n", "")
+  elif tag in ["br"]:
+    parseTextOut(tagElement, o, offset, "\n", "")
+  elif tag == "li":
+    parseListElement(tagElement, o, offset)
+  elif tag in ["ul", "ol"]:
+    parseList(tagElement, o, offset)
+  elif tag == 'table':
+    parseTable(tagElement, o, offset)
+  elif tag in ['tr']:
+    parseTableRow(tagElement, o, offset)
+  elif tag in ['td', 'th']:
+    parseTableCell(tagElement, o, offset)
+  else:
+    parseOther(tagElement, o, offset)
+
+def parseOther(tagElement, o, offset):
+  ## Don't actually do anything to the output here
+  print offset*"-" + "OTHER:"+tagElement.name+":open"
+  for anAttr in tagElement.attrs:
+    print offset*"-" + "OTHER:" + str(anAttr)
+
+  o.addText("")
+
+  ## Do we have any contents?
+  if tagElement.contents:
+    parseContents(tagElement.contents, o, offset+2)
+
+  # We're back from contents!
+  print offset*"-" + "OTHER:"+tagElement.name+":close"
+
+  o.addText("")
+
+def parseContents(contents, o, offset):
+  #print "NR in parseContents"
+  for c in contents:
+    ## Is this a comment?
+    if isinstance(c, element.Comment):
+      ## Don't do comments
+      print "Skipping a comment: ", c.string
+      continue
+    ## If it's a string, let's see what we do
+    if isinstance(c, element.NavigableString):
+      s = c.strip()
+      print offset*"-" + 'text: %s' % s
+      o.addText(s)
+    if isinstance(c, element.Tag):
+      ## It's a tag! Parse it
+      parseTag(c, o, offset)
+
+def parseTextOut(tagElement, o, offset, begin, end):
+
+  print offset*" " + tagElement.name + ":open"
+
+  o.addText(begin)
+
+  if tagElement.contents:
+    parseContents(tagElement.contents, o, offset+2)
+
+  o.addText(end)
+
+  print offset*" " + tagElement.name + ":close"
+
+def parseList(tagElement, o, offset):
+  print offset*" " + tagElement.name + ":open"
+  o.addText("\n\n")
+  if tagElement.name == "ul":
+    o.isUlStack.append(True)
+  else:
+    o.isUlStack.append(False)
+  o.liOffset += 2
+  if tagElement.contents:
+    parseContents(tagElement.contents, o, offset+2)
+  o.liOffset -= 2
+  o.isUlStack.pop()
+  print offset*" " + tagElement.name + ":close"
+
+def parseListElement(tagElement, o, offset):
+  print offset*" " + tagElement.name + ":open"
+  h = "#"
+  if o.isUlStack[ -1 ]:
+    h = "*"
+  o.addText(" "*o.liOffset + "* ")
+  if tagElement.contents:
+    parseContents(tagElement.contents, o, offset+2)
+  o.addText("\n\n")
+  print offset*" " + tagElement.name + ":close"
+
+def parseA(tagElement, o, offset):
+  print offset*"-" + 'A:open'
+  removeA = False
+  isWikiTarget = False
+  for anAttr in tagElement.attrs:
+    if anAttr[0] == 'name':
+      ## Don't do anything with names yet!
+      print offset*"-" + "ATTR:name"
+      removeA = True
+    elif anAttr[0] == "href":
+      target = anAttr[1]
+      if target.find("sortcol") >= 0:
+        removeA = True
+        continue
+      ## Is it an attachment?
+      if target.find("/cgi-bin/twiki/bin/viewfile") >= 0:
+        ## Get the part after "filename="
+        fname = target.split("filename=")[1]
+        o.addText(" attachment:%s " % fname)
+        return
+      ## Is it a pub attachment
+      if target.find("/pub/"+o.area+"/"+o.page) >= 0:
+        ## Get the last part
+        fname = target.split("/")[-1]
+        o.addText(" attachment:%s " % fname)
+        return
+      if target.find("/cgi-bin/twiki/bin/view/") >= 0:
+        ## It's a Wiki target
+        isWikiTarget = True
+        targetParts = target.split("/")
+        wikiTarget = targetParts[-1]
+        o.addText(" [[%s|" % wikiTarget)
+        o.addWikiLink(targetParts[-2],targetParts[-1])
+      else:
+        o.addText(' "')
+  ## Do we have any contents?
+  if tagElement.contents:
+    parseContents(tagElement.contents, o, offset+2)
+  ## Add the end part
+  if not removeA:
+    if isWikiTarget:
+      o.addText("]] ")
+    else:
+      target = "NR target test"
+      o.addText('":%s ' % target)
+  ## We're back from contents!
+  print offset*"-" + "A:close"
+
+### Main
+
 def main(args):
     ap = argparse.ArgumentParser()
     ap.add_argument('-f', '--file', 
@@ -99,12 +260,13 @@ def main(args):
         outfile = opts.outdir+'/' + title + '.redmine'
         print "Output file: ", outfile
         o = WikiFromSoup(document, title, outfile)
+        # We normally want to parse document body:
+        t = document.find('body')
+        parseTag(t, o, 0)
         o.writeOutRedmine()
         
 if __name__ == '__main__':
     # python conversion.py -f index.html 
     main(sys.argv[1:])
-
 else:
     print("this is a library")
-    
